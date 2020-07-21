de23e8e715065e8554d66196b73771844a72de3d
Ryan Whitmore <ryan@whitmorespace.com>
2020-07-21 12:45:13 -0700

    The participant list displays the mock data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Participants from './Participants';
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <header>
+    <h1>Owl App</h1>
+    </header>
+    <main className='App'>
+     <div>
+      <Participants 
+        name = {Participants.name}
+        
+      />
+     </div>
+    </main>
     </div>
   );
 }
